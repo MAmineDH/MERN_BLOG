0d172eb514206ceda2d49ab3a674c3e26746018d
MAmineDH <dhibmohamedamine1@gmail.com>
2024-06-08 12:32:53 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { BrowserRouter , Route , Routes } from "react-router-dom"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Porject from "./pages/Porject"
 
 export default function App() {
   return (
-    <div className='text-3xl'>
-      hello loser ! 
-    </div>
+   <BrowserRouter>
+   <Routes>
+    {/* i need to make the home page  */}
+    {/* <Route path="/" element={<Home/>}> */}
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/Signin" element={<SignIn/>}></Route>
+    <Route path="/SignUp" element={<SignUp/>}></Route>
+    <Route path="/Dashboard" element={<Dashboard/>}></Route>
+    <Route path="/Project" element={<Porject/>}></Route>
+   </Routes>
+   </BrowserRouter>
   )
 }
-    
---
+++ client/src/pages/Porject.jsx
@@ -0,0 +1,7 @@
+export default function Porject() {
+  return (
+    <div>
+        Project 
+    </div>
+  )
+}
